eb7ca002573b94efe3aed7776e6aed9fd3c69337
Tung <phandinhtung15@yahoo.com>
2017-10-14 22:24:35 +0000

    Write a page to edit, view and control all Equip

--- route/control.js
+++ route/control.js
@@ -11,25 +11,23 @@ module.exports = router;
 // app.use('/control', require('./route/control.js'));
 router.use(express.static("publics"));
 
-
+// view All Equip, choose to edit and control
 router.get("/", ensureAuth, function(req,res) {
   room.findByIdUser(req.user._id, (err, docs)=>{
     if (err) res.render('alert', {content: 'Database was errored'});
     else {
-      res.render('controlAEdit', {name: req.user.fullname, data: docs});
+      res.render('controlSocket', {name: req.user.fullname, data: docs});
     }
   });
 });
-//da tam tam
-router.get("/:id", ensureAuth, (req, res)=>{
-  // var id= req.params.id;
-  // console.log(id);
-  // room.findById(id, (err, doc)=>{
-  //   if (err) res.render('alert', {content: 'Database was errored'});
-  //   else{
-  //     console.log(doc);
-  //     res.json(doc);
-  //   }
-  // })
-  res.render('home');
-})
+
+//Page khong can nua - dung chung vs page view Equip
+// router.get("/:id", ensureAuth, (req, res)=>{
+//   var id= req.params.id;
+//   room.findById(id, (err, doc)=>{
+//     if (err) res.render('alert', {content: 'Database was errored'});
+//     else{
+//       res.render('socket',{name: req.user.fullname, data: docs})
+//     }
+//   })
+// })
