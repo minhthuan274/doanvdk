9da1db890dcd5f5931a7be64d4436c5fe734a69c
Tung <phandinhtung15@yahoo.com>
2017-10-14 15:40:47 +0000

    fixed error in Edit a Equipment

--- route/control.js
+++ route/control.js
@@ -1,17 +1,35 @@
 var express= require('express'),
     router= express.Router(),
     room= require('./room.js');
-module.exports = router;
-router.get("/", ensureAuth, function(req,res) {
-  room.findByIdUser(req.user.id, (err, docs)=>{
-    if (err) res.render('alert', {content: 'Database wasn\' errored'});
-    else{
-      res.render('control', {name: req.user.fullname, data: docs});
-    }
-  })
-  // res.render('control', {name: req.user.fullname});
-});
+
 function ensureAuth(req,res,next) {
   if(req.isAuthenticated()) return next();
-  res.redirect('login');
+  res.redirect('../login');
 }
+
+module.exports = router;
+// app.use('/control', require('./route/control.js'));
+router.use(express.static("publics"));
+
+
+router.get("/", ensureAuth, function(req,res) {
+  room.findByIdUser(req.user._id, (err, docs)=>{
+    if (err) res.render('alert', {content: 'Database was errored'});
+    else {
+      res.render('controlAEdit', {name: req.user.fullname, data: docs});
+    }
+  });
+});
+//da tam tam
+router.get("/:id", ensureAuth, (req, res)=>{
+  // var id= req.params.id;
+  // console.log(id);
+  // room.findById(id, (err, doc)=>{
+  //   if (err) res.render('alert', {content: 'Database was errored'});
+  //   else{
+  //     console.log(doc);
+  //     res.json(doc);
+  //   }
+  // })
+  res.render('home');
+})
--- route/outAuth.js
+++ route/outAuth.js
@@ -2,12 +2,27 @@ var express= require('express'),
     router= express.Router(),
     bodyParser= require('body-parser'),
     urlencodedParser= bodyParser.urlencoded({extended: false}),
-    //query= require('../route/query.js'),
     room= require('../route/room.js');
+
 module.exports = router;
+// app.use('/setup', require('./route/outAuth.js'));
+router.use(express.static("publics"));
+
+function ensureAuth(req,res,next) {
+  if(req.isAuthenticated()) return next();
+  res.redirect('../login');
+}
+
 router.get("/", ensureAuth, function(req,res) {
-  res.render('setting', {name: req.user.fullname});
+  room.findByIdUser(req.user._id, (err, docs)=>{
+    if(err) res.render('alert', {content: 'Database wasn\' errored'});
+    else{
+      if(docs.length>=5) res.render('alert', {content: 'You only have a maximum of 5 devices'});
+      else res.render('setting', {name: req.user.fullname});
+    }
+  })
 });
+
 router.post("/", urlencodedParser, ensureAuth, function(req,res) {
   var newRoom= new room({
     userId: req.user._id,
@@ -24,17 +39,33 @@ router.post("/", urlencodedParser, ensureAuth, function(req,res) {
   })
 });
 
-router.get("/edit/:id", ensureAuth, function(req, res) {
-  id= req.params.id;
-  room.findByIdUser(id, (err, doc)=>{
-    if(err) res.render('login',{content: 'Lỗi DATABASE'});
+router.get("/:id", ensureAuth, function(req, res) {
+  let id= req.params.id;
+  room.findById(id, (err, doc)=>{
+    if(err) res.render('alert',{content: 'Database was errored'});
     else{
-      res.render('editSetting',{data: doc});
+      res.render('editSetting',{data: doc, name: req.user.username});
     }
   });
-})
+});
 
-function ensureAuth(req,res,next) {
-  if(req.isAuthenticated()) return next();
-  res.redirect('login');
-}
+router.post("/:id", urlencodedParser, ensureAuth, (req,res)=>{
+  let id= req.params.id;
+  room.update(
+    {_id: id},
+    {$set: {
+      name: req.body.eqname,
+      output1: req.body.output1,
+      output2: req.body.output2,
+      output3: req.body.output3,
+      input1: req.body.input1,
+      input2: req.body.input2
+    }},
+    (err, doc)=>{
+      if (err) res.render('alert',{content: "Databse was error when update Equipment"});
+      else{
+        res.redirect('/control');
+      }
+    }
+  );
+});
